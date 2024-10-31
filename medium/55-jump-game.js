db2776764a9cb74f48ce948c0622f2f5bee4b80b
Logan Hosoda <35867188+LoganHosoda@users.noreply.github.com>
2024-10-31 12:14:06 -0700

    solved challenge with more optimal solution

--- medium/55-jump-game.js
+++ medium/55-jump-game.js
@@ -2,44 +2,55 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {
-    if (nums.length === 1) return true;
 
-    for (let i = 0; i < nums.length; i++) {
-        // loop backwards to see if jump can be made
-        if (nums[i] === 0) {
-            const checkJump = loopBack(nums, i);
-            if (checkJump[0]) i = checkJump[1];
-            else return false
-        }
-        // check if current index reaches the end, or is 0
-        if ((i + 1) + nums[i] >= nums.length) {
-            console.log(`nums[i]: ${nums[i]}, i: ${i}, nums.length: ${nums.length}`)
-            return true;
-        } else if (nums[i] === 0) {
-            const checkJump = loopBack(nums, i);
-            if (checkJump[0]) i = checkJump[1];
-            else return false
-        }
+ function canJump(nums) {
+    let max = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if (i > max) return false;
+        max = Math.max(max, i + nums[i]);
+        if (max >= nums.length - 1) return true;
+    }
+    return false;
+ }
 
-        console.log(`nums[i]: ${nums[i]}, i: ${i}`)
-    }
-    return true;
-};
+// var canJump = function(nums) {
+//     if (nums.length === 1) return true;
 
-function loopBack(nums, i) {
-    let foundJump = false;
-    for (let j = i - 1; j >= 0; j--) {
-        if (j + nums[j] > i) {
-            i = j + nums[j];
-            foundJump = true;
-            return [foundJump, i]
-        }
-    }
-    if (!foundJump) {
-        return [foundJump, i];
-    }
-}
+//     for (let i = 0; i < nums.length; i++) {
+//         // loop backwards to see if jump can be made
+//         if (nums[i] === 0) {
+//             const checkJump = loopBack(nums, i);
+//             if (checkJump[0]) i = checkJump[1];
+//             else return false
+//         }
+//         // check if current index reaches the end, or is 0
+//         if ((i + 1) + nums[i] >= nums.length) {
+//             console.log(`nums[i]: ${nums[i]}, i: ${i}, nums.length: ${nums.length}`)
+//             return true;
+//         } else if (nums[i] === 0) {
+//             const checkJump = loopBack(nums, i);
+//             if (checkJump[0]) i = checkJump[1];
+//             else return false
+//         }
+
+//         console.log(`nums[i]: ${nums[i]}, i: ${i}`)
+//     }
+//     return true;
+// };
+
+// function loopBack(nums, i) {
+//     let foundJump = false;
+//     for (let j = i - 1; j >= 0; j--) {
+//         if (j + nums[j] > i) {
+//             i = j + nums[j];
+//             foundJump = true;
+//             return [foundJump, i]
+//         }
+//     }
+//     if (!foundJump) {
+//         return [foundJump, i];
+//     }
+// }
 
 // Input: array of nums, positive only, no floats
 // Output: true / false
